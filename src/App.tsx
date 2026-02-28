4b14e96fdadf1498ea6f6829c59541329ea92281
Noah Boone <noahboone@Noahs-Laptop.local>
2026-02-27 17:04:17 -0700

    fix: styling updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 
+import '@fontsource/montserrat/700.css';
+import '@fontsource/montserrat/800.css';
+import '@fontsource/lato/400.css';
+import '@fontsource/lato/700.css';
 import './App.css';
 
 function App() {
